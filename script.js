216e32e290b63805c74e5d7d232ef15fd6fbbece
Nayabsaleem <nayabsaleem280@gmail.com>
2024-12-16 19:45:58 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let btn = document.getElementById("btn");
+
+btn.onclick = () =>{
+    if (document.body.style.backgroundColor === "white"){
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "black";
+        btn.textContent = "Switch to light mode";
+    }
+    else {
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "white";
+        btn.textContent = "Switch to dark mode"
+    }
+}
